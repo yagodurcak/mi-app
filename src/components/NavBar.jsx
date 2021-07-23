fd5aff4a64fbd0ba0e53597f72b1ff9a1db2e4f2
yagodurcak <mauridurcak@gmail.com>
2021-07-23 18:35:59 -0300

    Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import "./navBar.css";
+
+function NavBar() {
+
+
+    return(
+
+        <div>            
+            <nav className= "NavBar">
+                <h1 className="nombre">Tienda Online</h1>
+                <div className="menu">
+                    <a href="">Nosotros</a>
+                    <a href="">Servicios</a>
+                    <a href="">Contacto</a>
+                </div>
+            </nav>
+        </div>
+
+        
+    )
+    
+    
+}
+
+export default NavBar;
