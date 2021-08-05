54a479a68ad3eb011b03f48c162a38003a9acef0
yagodurcak <mauridurcak@gmail.com>
2021-08-05 20:46:48 -0300

    listo

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,16 +4,36 @@ import React, {useState, useEffect} from 'react';
 const ItemCount = () => {
   const [contador, setContador] = useState(1);
 
+  let Restar = () => {
+    setContador(contador - 1)
+  }
+  let Sumar = () => {
+    setContador(contador + 1)
+  }
+  
+
   
   const Stock = (stock, contador) => {
       
     if (stock == contador) {
 
-        return stock
-                
-    } else { 
+    
+
+        Sumar = () => {
+            setContador(contador)
+          };
+
+
+       
+
+        // setContador("No hay mas Stock!");
+
+        return "Numero: " + contador
+
+        } else { 
+    
         
-        return contador
+        return "Numero: " + contador
     }
   }
   
@@ -31,21 +51,20 @@ const ItemCount = () => {
               <div className="card d-flex flex-row justify-content-between">
                 <a
                   className="btn btn-primary"
-                  onClick={() => setContador(contador - 1)}
+                  onClick={() => Restar() }
                 >
                   - 
                 </a>
-                <h4>Numero:{contador}</h4>                
+                <h4>{Stock(10, contador)}</h4>                
                 <a
                   className="btn btn-primary"
-                  onClick={() => setContador(contador + 1)}
+                  onClick={() => Sumar()}
                 >
                   +
                 </a>
               </div>
             </div>
 
-            <h3>{Stock(10, contador)}</h3>
 
           </div>
         </div>
